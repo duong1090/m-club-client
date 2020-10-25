3a7c9ce4b5c8a6af93c1051faf93d447687b1c77
Duong Pham Thai <duong1090@gmail.com>
2020-10-25 12:14:12 +0700

    [F] create Login Page

---
+++ container/component/logIn/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { View, TextInput, Button } from "react-native";
+
+const Login = () => {
+  return (
+    <View>
+      <TextInput />
+      <TextInput />
+      <Button />
+    </View>
+  );
+};
