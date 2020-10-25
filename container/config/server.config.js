85d02eea03f40fc6bfb0ae80e1d83d4c371985b9
Duong Pham Thai <duong1090@gmail.com>
2020-10-25 12:13:09 +0700

    config server

---
+++ container/config/server.config.js
@@ -0,0 +1,10 @@
+const Config = () => {
+  const development = {
+    // API_URL: "https://10.0.2.2:44373/api/",
+    // API_URL: "http://113.172.219.43:1000/api/",
+    API_URL: "http://172.20.10.4:8888/api/",
+  };
+  return development;
+};
+
+export default Config();
