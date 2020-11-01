60a18a7a7940959035b22b57aa82c9ac3a3ea501
Duong Pham Thai <duong1090@gmail.com>
2020-11-01 23:48:27 +0700

    config local storage

--- container/constant/storage.js
+++ container/constant/storage.js
@@ -1,1 +1,2 @@
 export const API_TOKEN = "API_TOKEN";
+export const IS_FIRST_TIME = "IS_FIRST_TIME";
--- container/utils/storage.js
+++ container/utils/storage.js
@@ -1,11 +1,18 @@
-import { AsyncStorage } from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-community/async-storage";
 
 export const setItem = async (...args) => {
   await AsyncStorage.setItem(...args);
 };
 
 export const getItem = async (...args) => {
-  return await AsyncStorage.getItem(...args);
+  try {
+    const value = await AsyncStorage.getItem(...args);
+    if (value !== null) {
+      // value previously stored
+    }
+  } catch (e) {
+    // error reading value
+  }
 };
 
 export const removeItem = async (...args) => {
