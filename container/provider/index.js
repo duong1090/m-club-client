c9eb79577a3d58c825db578be2d1ce30bd590a15
Duong Pham Thai <duong1090@gmail.com>
2020-12-14 16:41:49 +0700

    fix provider index

--- container/provider/index.js
+++ container/provider/index.js
@@ -1,37 +1,30 @@
-import React, { useEffect, useState } from "react";
-import {
-  RecoilRoot,
-  useRecoilTransactionObserver_UNSTABLE,
-  useSetRecoilState,
-} from "recoil";
-import AsyncStorage from "@react-native-community/async-storage";
-import { setItem, getAllItem } from "container/utils/storage";
+import React, { useEffect } from "react";
+import { RecoilRoot } from "recoil";
 import IntlMainProvider from "./intlProvider";
-import rootAtom from "../recoil/state";
 
 import { LogBox } from "react-native";
 
-const PersistenceObserver = () => {
-  useRecoilTransactionObserver_UNSTABLE(({ snapshot }) => {
-    for (const modifiedAtom of snapshot.getNodes_UNSTABLE({
-      isModified: true,
-    })) {
-      const atomLoadable = snapshot.getLoadable(modifiedAtom);
-      console.log(
-        "PersistenceObserver::::",
-        modifiedAtom.key,
-        atomLoadable.contents
-      );
-      if (atomLoadable.state === "hasValue") {
-        setItem(
-          modifiedAtom.key,
-          JSON.stringify({ value: atomLoadable.contents })
-        );
-      }
-    }
-  });
-  return null;
-};
+// const PersistenceObserver = () => {
+//   useRecoilTransactionObserver_UNSTABLE(({ snapshot }) => {
+//     for (const modifiedAtom of snapshot.getNodes_UNSTABLE({
+//       isModified: true,
+//     })) {
+//       const atomLoadable = snapshot.getLoadable(modifiedAtom);
+//       console.log(
+//         "PersistenceObserver::::",
+//         modifiedAtom.key,
+//         atomLoadable.contents
+//       );
+//       if (atomLoadable.state === "hasValue") {
+//         setItem(
+//           modifiedAtom.key,
+//           JSON.stringify({ value: atomLoadable.contents })
+//         );
+//       }
+//     }
+//   });
+//   return null;
+// };
 
 const MainProvider = (props) => {
   const { children } = props;
