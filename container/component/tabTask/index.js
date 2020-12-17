a952c31076a40dd3fdc180e3e9c5ff1dd2415aaa
Khang Le <khangldit@gmail.com>
2020-12-17 16:10:35 +0700

    no message

--- container/component/tabTask/index.js
+++ container/component/tabTask/index.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { View,StyleSheet } from "react-native";
+import HeaderInfo from "./HeaderInfo.js";
+import ListTask from "./ListTask.js";
+import {
+  scale,
+  shadow,
+} from "container/variables/common";
 
 const TabTask = (props) => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <HeaderInfo />
+      <ListTask />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: "#eee",
+      height: '100%',
+      ...shadow,
+  },
+});
 
 export default TabTask;
